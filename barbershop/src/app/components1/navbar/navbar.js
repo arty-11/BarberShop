6b3e2e0cfeb37725d28b57777c66992d23fd2187
Andy Agyei-Dwumah <andydwumah@gmail.com>
2024-02-05 23:37:43 +0000

    Added scroll styling to navbar

--- barbershop/src/app/components1/navbar/navbar.js
+++ barbershop/src/app/components1/navbar/navbar.js
@@ -1,6 +1,11 @@
+'use client';
+
+//imports
 import Link from "next/link";
 import styles from './navbar.module.css';
+import { useEffect, useState } from "react";
 
+//navlinks
 const navLinks = [
     { id: "1", name: "About", path: "#about" },
     { id: "2", name: "Services", path: "#services" },
@@ -8,11 +13,30 @@ const navLinks = [
     { id: "4", name: "Book", path: "/" }
 ];
 
+
 export default function Navbar() {
-    return ( 
-        <header>
+    const [changeHeadingColour, setChangeHeadingColour] = useState(false);
+
+    const toggleScroll = () => {
+        let scrollY = window.scrollY;
+        if (scrollY <= 196) {
+            setChangeHeadingColour(false);
+        } else {
+            setChangeHeadingColour(true);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', toggleScroll);
+        return () => {
+            window.removeEventListener('scroll', toggleScroll);
+        }
+    })
+
+    return (
+        <header className={changeHeadingColour ? [styles.NavHeaderTransparent, styles.NavHeader].join(" ") : [styles.NavHeaderColour, styles.NavHeader].join(" ")}>
             <nav aria-label="primary">
-                <ul className={styles.NavULFlexContainer}>
+                <menu className={styles.NavUlFlexContainer}>
                     {navLinks.map(navLink => (
                         <li key={navLink.id} className={styles.NavLiFlexItems}>
                             <Link href={navLink.path}>
@@ -20,7 +44,7 @@ export default function Navbar() {
                             </Link>
                         </li>
                     ))}
-                </ul>
+                </menu>
             </nav>
         </header>
      );
