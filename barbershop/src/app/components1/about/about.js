4d2baef6dd5fbf9e3f16e96735efb93e1d43a9ce
Andy Agyei-Dwumah <andydwumah@gmail.com>
2024-02-10 22:58:59 +0000

    Add two div tags to About section

--- barbershop/src/app/components1/about/about.js
+++ barbershop/src/app/components1/about/about.js
@@ -7,7 +7,9 @@ import AboutImg from './aboutImg.js';
 
 export default function About() {
   return (
-    <section aria-labelledby="about">
+    <section aria-labelledby="about" className={styles.aboutSectionContainer}>
+      <div className={styles.divRight}></div>
+      <div className={styles.divLeft}></div>
       <h2 id="about" className={styles.AboutMainHeading}>About Us</h2>
       <div className={styles.AboutFlexContainer}>
         <div className={styles.AboutContainerText}>
