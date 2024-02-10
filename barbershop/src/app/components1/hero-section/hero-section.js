09c1727caa1e3cd253354be4b436d21812f9cff3
Andy Agyei-Dwumah <andydwumah@gmail.com>
2024-02-10 19:06:39 +0000

    Update hero section

--- barbershop/src/app/components1/hero-section/hero-section.js
+++ barbershop/src/app/components1/hero-section/hero-section.js
@@ -3,29 +3,37 @@
 import styles from './hero-section.module.css';
 import { ParallaxBanner } from 'react-scroll-parallax';
 
+const layerStyle = {
+    position: "absolute",
+    top: "0"
+}
+
 export default function HeroSection() {
     return (
         <section>
             <ParallaxBanner
                 className={styles.parallaxBanner}
                 layers={[
-                    { image: '/Barber-imgs/picture-1.jpg', speed: -80, scale: [0.6, 1.4]},
+                    { image: '/Barber-imgs/picture-1.jpg', speed: -80, scale: [0.6, 1.4], style: layerStyle },
                     {
                         speed: -20,
+                        style: layerStyle,
                         children: (
-                            <div className={styles.textDiv}
-                            >
-                                <div>
-                                    <h1 className={styles.textH1}>Hair Lounge</h1>
-                                    <h3 className={styles.textH3}>Unisex Salon</h3>
-                                    <p className={styles.textP}>London | Dagenham</p>
+                            <div className={styles.textDivContainer}>
+                                <div className={styles.textDiv}>
+                                    <div>
+                                        <h1 className={styles.textH1}>Hair Lounge</h1>
+                                        <h3 className={styles.textH3}>Unisex Salon</h3>
+                                        <p className={styles.textP}>London | Dagenham</p>
+                                    </div>
+                                    <div>
+                                        <button className={styles.textButton}>Book Now</button>
+                                    </div>
+                                    </div>
                                 </div>
-                                <div>
-                                    <button className={styles.textButton}>Book Now</button>
-                                </div>
-                            </div>
                         ),
                     },
+                    // { speed: 1, children: <div className={styles.whiteDiv}></div> }
                 ]}
             >
             </ParallaxBanner>
