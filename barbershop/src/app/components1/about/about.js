9d75ddf99e8737e47ab7811ca05297be430e0da5
Andy Agyei-Dwumah <andydwumah@gmail.com>
2024-02-10 22:33:50 +0000

    Add aboutImg and aboutText component

--- barbershop/src/app/components1/about/about.js
+++ barbershop/src/app/components1/about/about.js
@@ -1,5 +1,9 @@
-import Image from 'next/image';
+//imports
 import styles from './about.module.css';
+
+//components
+import AboutText from './aboutText.js';
+import AboutImg from './aboutImg.js';
 
 export default function About() {
   return (
@@ -7,28 +11,10 @@ export default function About() {
       <h2 id="about" className={styles.AboutMainHeading}>About Us</h2>
       <div className={styles.AboutFlexContainer}>
         <div className={styles.AboutContainerText}>
-          <h3 className={[styles.AboutHeading, styles.MarginLeft].join(" ")}>
-            Why Hair Lounge?
-          </h3>
-          <p
-            className={[styles.MarginLeft, styles.ParaMarginTop, styles.ParaMarginRight, styles.AboutText].join(" ")}>
-            Well, what makes Hair Lounge unique from other barber shops is our staff expertise &#40;each barber has 12+ years&#41;, haircuts &#40;we offer quality and a wide range of trims&#41; and &apos;barber banter&apos;.
-          </p>
-          <p
-            className={[styles.MarginLeft, styles.ParaMarginTop, styles.ParaMarginRight, styles.AboutText].join(" ")}>
-            A relaxing and chill ambiance. Regardless of gender, race, or any other factor, we strive to create enduring relationships with all our customers. At Hair Lounge we strive to boost confidence and bring out the best in every client. Getting a haircut is just one part of what we do.
-          </p>
+          <AboutText styles={styles} />
         </div>
         <div className={styles.AboutContainerImage}>
-          <Image
-            src='/Barber-imgs/picture-6.jpg'
-            fill={true}
-            quality={100}
-            sizes='(max-width:800px) 30vw, 40vw'
-            alt="Barber cutting hair"
-            className={styles.AboutImage}
-            priority
-          />
+          <AboutImg styles={styles} />
         </div>
       </div>
     </section>
---
+++ barbershop/src/app/components1/about/aboutImg.js
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+export default function AboutImg({ styles }) {
+  return (
+    <Image
+        src='/Barber-imgs/picture-6.jpg'
+        fill={true}
+        quality={100}
+        sizes='(max-width:800px) 30vw, 40vw'
+        alt="Barber cutting hair"
+        className={styles.AboutImage}
+        priority
+          />
+  )
+}
