72e30374bc131d3f79a2923c0f81b2d3ba1d223f
Andy Agyei-Dwumah <andydwumah@gmail.com>
2024-02-11 21:30:02 +0000

    Install fontawesome

--- barbershop/src/app/components1/where-to-find-us/where-to-find-us.js
+++ barbershop/src/app/components1/where-to-find-us/where-to-find-us.js
@@ -1,5 +1,24 @@
+//imports
+import styles from "./where-to-find-us.module.css";
+
+//components
+import WtfuImg from "./wtfuImg";
+import WtfuText from "./wtfuText";
+
 export default function WhereToFindUs() {
   return (
-    <div className="headingStyle">Where To Find Us</div>
+    <section aria-labelledby="where-to-find-us">
+      <h2 id="where-to-find-us" className="headingStyle">
+      Where To Find Us
+      </h2>
+      <div className={styles.whereToFindUsFlexContainer}>
+        <div className={styles.whereToFindUsTextContainer}>
+          <WtfuText />
+        </div>
+        <div className={styles.whereToFindUsImgContainer}>
+          <WtfuImg />
+        </div>
+      </div>
+    </section>
   )
 }
