1ec01504f08da1ce0aa90e20eadad112a9ac64e7
Andy Agyei-Dwumah <andydwumah@gmail.com>
2024-03-10 19:44:33 +0000

    Add media queries to meet the team section

--- barbershop/src/app/components1/meet-the-team/socialMedia.js
+++ barbershop/src/app/components1/meet-the-team/socialMedia.js
@@ -5,9 +5,9 @@ import TwitterIcon from "./twitterIcon"
 export default function SocialMedia({ containerNameHover, styles }) {
     return (
         <div className={containerNameHover ? [styles.socialMediaFlexContainer, styles.fullWidth].join(" ") : styles.socialMediaFlexContainer}>
-            <FBIcon iconColour="white" />
-            <IgIcon iconColour="white" />
-            <TwitterIcon iconColour="white" />
+            <FBIcon iconColour="white" styles={styles} />
+            <IgIcon iconColour="white" styles={styles} containerNameHover={containerNameHover} />
+            <TwitterIcon iconColour="white" styles={styles} />
         </div>
   )
 }
