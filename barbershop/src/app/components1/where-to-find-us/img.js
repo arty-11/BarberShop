1e36fb972e114ee166d462da877ae2056fc34582
Andy Agyei-Dwumah <andydwumah@gmail.com>
2024-03-15 15:17:12 +0000

    Add picture 9 to public folder

--- barbershop/src/app/components1/where-to-find-us/img.js
+++ barbershop/src/app/components1/where-to-find-us/img.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Img({ styles }) {
   return (
     <Image
-        src='/Barber-imgs/picture-8.jpg'
+        src='/Barber-imgs/picture-9.jpg'
         fill={true}
         quality={100}
         sizes='(max-width:800px) 30vw, 40vw'
